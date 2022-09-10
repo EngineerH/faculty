099d6f8303c00df426e972be7b76bd946e3e4334
Morhlon Harris <morhlon@Morhlons-Mini.home>
2022-09-09 20:59:20 -0400

    added loop through csv rows

--- faculty.py
+++ faculty.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
-df = pd.read_excel("faculty.xlsx")
-# print(df.columns[0])
-print(df.head())
+df = pd.read_csv("faculty.csv",na_values="missing")
+column_names = [col for col in df.columns]
+item = (df["Monday Morning"][0])
+location = item.split("-")[0].strip()
+faculty = item.split("-")[1].strip()
+print(faculty,location)
+
+for name in column_names:
+    for row in df[name]:
+        item = row.split("-")
+        location = item[0].strip()
+        faculty = item[1].strip()
+        print(f"On {name} Dr.{faculty} is in {location}")
+# print(df.head())
